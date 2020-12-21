2d4b7d408cda8128fa732016b25a3f7fc656ecf5
music5tand <ggaimt139@gmail.com>
2020-12-21 23:18:56 +0900

    I forking hate sort
    f off

--- week3/week3_1931.cpp
+++ week3/week3_1931.cpp
@@ -5,31 +5,31 @@
 using namespace std;
 
 
-bool cmp(const pair<int,int> &a, const pair<int,int> &b){
-    if (a.second != b.second){
-        return a.second < b.second;
+bool cmp(const pair<int,int>&a, const pair<int,int>&b){
+    if (a.second == b.second){
+        return a.first < b.first;
     }else{
-        a.first < b.first;
+        return a.second < b.second;
     }
 
 }
 
 
 int main(){
-    int cnt, pos, N;
-
-    cin>>N;
-    vector<pair<int,int>> meeting(N);
+    int cnt = 0, pos = 0, N;
+    pair<int,int> meeting[100000];
 
     // 0. input
+    cin>>N;
     for(int i = 0; i<N; i++){
         cin>>meeting[i].first>>meeting[i].second;  
     }
     
     //1 . sort by ending time.
-    sort(&meeting[0], &meeting[N],cmp);
+    sort(meeting, meeting+N, cmp);
 
 
+/*
     //2. count
     for (int i = 0; i < N ; i++){
         if( pos<= meeting[i].first){
@@ -37,7 +37,11 @@ int main(){
             cnt++;
         }
     }
+*/    
+    for (int i = 0; i < N ; i++){
+        cout<<meeting[i].first<<" "<<meeting[i].second<<'\n';
+    }
 
-    cout<<cnt<<'\n';
+    // cout<<cnt<<'\n';
     return 0;
 }
